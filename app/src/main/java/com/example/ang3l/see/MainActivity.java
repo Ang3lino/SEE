1be17004cd3cfed330134810a8234b1554bf21f3
ang3lino <sigma271@outlook.com>
2018-05-26 15:33:01 -0500

    cambios hecho otra vez

--- app/src/main/java/com/example/ang3l/see/MainActivity.java
+++ app/src/main/java/com/example/ang3l/see/MainActivity.java
@@ -4,18 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnRegister;
+    private Button btnLogin;
+    TextView txtRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnRegister = findViewById(R.id.btn_register);
-        btnRegister.setOnClickListener(view -> { // we'll pass to CreateAccountActivity through intent
+        txtRegister = findViewById(R.id.txt_register);
+        txtRegister.setOnClickListener(view -> { // we'll pass to CreateAccountActivity through intent
+            startActivity(new Intent(this, CreateAccountActivity.class));
+        });
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(view -> { // we'll pass to CreateAccountActivity through intent
             startActivity(new Intent(this, CreateAccountActivity.class));
         });
     }
