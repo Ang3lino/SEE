8a72adb3aa96da6c0ed7818db6e8a0d99265c7d1
Ang3lino <sigma271@outlook.com>
2018-05-27 11:30:37 -0500

    Nueva actividad casi acabada

--- app/src/main/java/com/example/ang3l/see/MainActivity.java
+++ app/src/main/java/com/example/ang3l/see/MainActivity.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnLogin;
+    private Button btnJumper;
     TextView txtRegister;
 
     @Override
@@ -20,9 +22,15 @@ public class MainActivity extends AppCompatActivity {
         txtRegister.setOnClickListener(view -> { // we'll pass to CreateAccountActivity through intent
             startActivity(new Intent(this, CreateAccountActivity.class));
         });
+
         btnLogin = findViewById(R.id.btn_login);
         btnLogin.setOnClickListener(view -> { // we'll pass to CreateAccountActivity through intent
-            startActivity(new Intent(this, CreateAccountActivity.class));
+            Toast.makeText(this, "Por implementar v:", Toast.LENGTH_LONG).show();
+        });
+
+        btnJumper = findViewById(R.id.btn_jumper);
+        btnJumper.setOnClickListener(view -> { // we'll pass to CreateAccountActivity through intent
+            startActivity(new Intent(this, AdministratorOptionsActivity.class));
         });
     }
 
