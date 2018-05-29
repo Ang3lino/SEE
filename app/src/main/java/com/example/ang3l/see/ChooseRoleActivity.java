e14fb83a4d0c4a519435b285bc87d7f38c928b81
Ang3lino <sigma271@outlook.com>
2018-05-28 20:13:00 -0500

    Nota personal: cada que algo funcione lo tienes que subir, porque luego hay bugs de android que no percibe!

---
+++ app/src/main/java/com/example/ang3l/see/ChooseRoleActivity.java
@@ -0,0 +1,18 @@
+package com.example.ang3l.see;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class ChooseRoleActivity extends AppCompatActivity {
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_role);
+
+        toolbar = findViewById(R.id.toolbar_choose_role);
+        setSupportActionBar(toolbar); // le damos soporte a los pobres
+    }
+}
