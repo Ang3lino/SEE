203d915f6989381b8141578c250bc9a46469f475
Ang3lino <sigma271@outlook.com>
2018-06-04 16:23:02 -0500

    Everthing working

--- app/src/main/java/com/example/ang3l/see/AdministratorOptionsActivity.java
+++ app/src/main/java/com/example/ang3l/see/AdministratorOptionsActivity.java
@@ -1,14 +1,20 @@
 package com.example.ang3l.see;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 
 public class AdministratorOptionsActivity extends AppCompatActivity {
+    CardView cardAddPostulant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administrator_options);
+
+        cardAddPostulant = findViewById(R.id.cardview_add_postulant);
+        cardAddPostulant.setOnClickListener(v -> startActivity(new Intent(this, WatchPostulantActivity.class)));
     }
 
 }
